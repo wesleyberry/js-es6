30e231b6ff13959f77f7c9d4c348160ee5bdcf15
Wesley Berry <wesleyberry52@gmail.com>
2019-12-20 12:20:45 -0600

    Arrow functions

--- index.js
+++ index.js
@@ -69,8 +69,20 @@
 
 // -----------------------------------------------------------
 // rest Operator
-function add(...nums) {
-    console.log(nums);
-}
+// function add(...nums) {
+//     console.log(nums);
+// }
 
+// add(4, 5, 7, 8, 12);
+
+// -----------------------------------------------------------
+// Arrow Functions
+add = (...nums) => {
+    // let total = nums.reduce(function (x, y) {
+    //     return x + y;
+    // });
+    // vs
+    let total = nums.reduce((x, y) => x + y);
+    console.log(total);
+}
 add(4, 5, 7, 8, 12);
