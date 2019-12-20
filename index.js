8f5b31d1273db0a7592f52c25c7f45569ddc55d2
Wesley Berry <wesleyberry52@gmail.com>
2019-12-20 11:56:14 -0600

    destructuring arrays and objects

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-// Destructuring
-const personalInfo = {
-    firstName: 'Wesley',
-    lastName: 'Berry',
-    city: 'San Marcos',
-    state: 'TX',
-    zipCode:'78666'
-};
+// Destructuring Objects
+// const personalInfo = {
+//     firstName: 'Wesley',
+//     lastName: 'Berry',
+//     city: 'San Marcos',
+//     state: 'TX',
+//     zipCode:'78666'
+// };
 
-const {firstName, lastName} = personalInfo;
+// const {firstName, lastName} = personalInfo;
 
-console.log(`${firstName} ${lastName}`);
+// console.log(`${firstName} ${lastName}`);
+// ------------------------------------------------------
+// Destructuring Arrays
+let [firstName, middleName, lastName] = ['Wesley', 'Coding Genius', 'Berry'];
+
+lastName = 'Gordon';
+
+console.log(firstName)
+console.log(middleName)
+console.log(lastName)
+// -------------------------------------------------------
+// Object Literal
