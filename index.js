54f4098b492bdddaa89ad5ea98ca4d405b536cf1
Wesley Berry <wesleyberry52@gmail.com>
2019-12-20 12:13:25 -0600

    spread operator

--- index.js
+++ index.js
@@ -60,3 +60,9 @@
 
 // -----------------------------------------------------------
 // Spread Operator
+let example1 = [1,2,3,4,5,6];
+
+let example2 = [...example1];
+example2.push(true);
+
+console.log(example2);
